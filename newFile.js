cf2f4befaaa5203540f0642cfc4dc3ec7aa4721e
dylrus <63077541+dylrus@users.noreply.github.com>
2020-12-17 15:57:33 +1100

    Add user Object
    Added in user Object with data and fullname Method

--- newFile.js
+++ newFile.js
@@ -1,2 +1,12 @@
+const currentUser = {
+  firstName: "Dylan",
+  lastName: "Russell",
+  fullName: function () {
+    return this.firstName + " " + this.lastName;
+  },
+};
+
+currentUser.fullname();
+
 const testFunction = () =>
-  console.log("Hello you big dumb dumbs this is a github test file");
+  console.log("Hello you courteous person this is a github test file");
